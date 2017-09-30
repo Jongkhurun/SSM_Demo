7bdf5104e16bce891156a9677ed50049ab0f9925
HaitaoHu <2355394037@qq.com>
2017-09-30 09:41:09 +0800

    init

---
+++ src/main/java/main/App.java
@@ -0,0 +1,24 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Author 蟑螂
+ * @Create 2017-09-29 15:58
+ */
+/*@Configuration/*标识这个类可以使用Spring IoC容器作为bean定义的来源。@Bean注解告诉Spring，一个带有@Bean的注解方法将返回一个对象，该对象应该被注册为在Spring应用程序上下文中的bean。*/
+/*@EnableAutoConfiguration/*能够自动配置spring的上下文，试图猜测和配置你想要的bean类，通常会自动根据你的类路径和你的bean定义自动配置。*/
+/*@ComponentScan/*会自动扫描指定包下的全部标有@Component 的类，并注册成bean，当然包括@Component下的子注解@Service,@Repository,@Controller*/
+@SpringBootApplication/*@Configuration,@EnableAutoConfiguration,@ComponentScan 统一注解*/
+@EnableCaching/*缓存支持*/
+@EnableTransactionManagement/*事物支持*/
+public class App {
+    public static void main(String[] args) {
+        System.out.println("开始启动");
+        SpringApplication.run(App.class, args);
+        System.out.println("启动成功");
+    }
+}
