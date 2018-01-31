bd8beb5bcc85493e0197b86341d2363f6f288f77
HaitaoHu <2355394037@qq.com>
2018-01-31 20:06:11 +0800

    添加参数模型实例

--- src/main/java/com/ssm/demo/domain/object/vo/groups/Fourth.java
+++ src/main/java/com/ssm/demo/domain/object/vo/groups/Fourth.java
@@ -1,9 +1,9 @@
 package com.ssm.demo.domain.object.vo.groups;
 
 /**
- * 验证分组 - 贰
+ * 验证分组 - 肆
  * @author 懒♂癌
- * @date 2018-01-31 13:38
+ * @date 2018-01-31 16:19
  */
-public interface Second {
+public interface Fourth {
 }
--- src/main/java/com/ssm/demo/domain/object/vo/groups/Third.java
+++ src/main/java/com/ssm/demo/domain/object/vo/groups/Third.java
@@ -1,9 +1,9 @@
 package com.ssm.demo.domain.object.vo.groups;
 
 /**
- * 验证分组 - 贰
+ * 验证分组 - 叁
  * @author 懒♂癌
- * @date 2018-01-31 13:38
+ * @date 2018-01-31 16:19
  */
-public interface Second {
+public interface Third {
 }
